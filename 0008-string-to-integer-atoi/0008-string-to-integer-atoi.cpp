968b9d1b047285d782d4c1eeec00a8f43f645cb3
Jagrit Goyal <125178986+jagrit-goyal@users.noreply.github.com>
2024-07-14 00:04:23 +0530

    Time: 2 ms (59.90%), Space: 9.1 MB (7.49%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0;
+        for(;i<s.length()&&s[i]==' ';i++);
+
+        int sign = 1;
+        if(i<s.length()&& (s[i]=='+' || s[i]=='-'))
+        {
+            sign = (s[i]=='-')? -1 : 1;
+            i++;
+        }
+        long num =0;
+
+        while(i<s.length() && isdigit(s[i]) && num<=INT_MAX)
+        {
+            num = num*10 + (s[i++] - '0');
+            // it helps in making the numeric character into integer value
+        }
+        // for rounding off wala part
+        num = max(sign*num , (long)INT_MIN);
+        num = min(num , (long)INT_MAX);
+        return (int)num;
+    }
+};
